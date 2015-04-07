9a58d1f7a5fb2267b1a75cdbce4fb3f280c3c874
hmtri92 <hmtri92@gmail.com>
2015-04-07 16:18:02 +0700

    Security demo
    Status: error

--- src/main/java/com/csc/controller/LoginController.java
+++ src/main/java/com/csc/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.csc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
@@ -10,5 +11,28 @@ public class LoginController {
 	@RequestMapping (value = "/login", method = {RequestMethod.GET, RequestMethod.POST})
 	public String doLogin() {
 		return "login";
+	}
+	
+	@RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
+	public ModelAndView welcomePage() {
+ 
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Hello World");
+		model.addObject("message", "This is welcome page!");
+		model.setViewName("hello");
+		return model;
+ 
+	}
+ 
+	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
+	public ModelAndView adminPage() {
+ 
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Hello World");
+		model.addObject("message", "This is protected page!");
+		model.setViewName("admin");
+ 
+		return model;
+ 
 	}
 }
